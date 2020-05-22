cd792366ff24f601ab9d6df180b213925f65b1c2
mightykim91 <mightykim91@naver.com>
2020-05-22 01:53:16 +0000

    updated algorithm for recommendation function

--- movies/views.py
+++ movies/views.py
@@ -3,8 +3,9 @@ from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import Movie
-import operator
+import random
 from random import shuffle
+
 
 # Create your views here.
 def index(request):
@@ -13,21 +14,58 @@ def index(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
+
+    #장르 추출
+    genres = set()
+    if request.user.is_authenticated:
+        user = request.user
+        like_movies = user.like_movies.all()
+
+        for movie in like_movies:
+            for genre in list(movie.genres.all().values('name')):
+                genres.add(genre['name'])
+
+    #추천
+    not_watched = []
+    recommendation = []
+    for movie in movies:
+        if movie not in user.like_movies.all():
+            not_watched.append(movie)
+    #print(not_watched)
+
+    while len(recommendation) < 10:
+        rand_int = random.randrange(len(movies))
+        shuffle(not_watched)
+        not_watched_movie_genres = []
+        for j in list(not_watched[rand_int].genres.all().values('name')):
+            not_watched_movie_genres.append(j['name'])
+        #print(not_watched_movie_genres)
+        for temp_genre in not_watched_movie_genres:
+            if temp_genre in genres:
+                recommendation.append(not_watched[rand_int])
+                break
+
+    print(recommendation)
+
     context = {
 
         'movies':movies,
         'page_obj':page_obj,
+        'recom_top_five': recommendation[:5],
+        'recom_bot_five': recommendation[5:],
     }
 
 
     return render(request, 'movies/index.html', context)
 
+
 def detail(request, movie_pk):
     movie = Movie.objects.get(pk=movie_pk)
     user = request.user
     color = 'black'
-    if user.like_movies.filter(pk=movie_pk).exists():
-        color = 'crimson'
+    if user.is_authenticated:
+        if user.like_movies.filter(pk=movie_pk).exists():
+            color = 'crimson'
 
     genres = []
     for genre in list(movie.genres.all().values('name')):
